c509e95fa7927c3d3a4a2b6700d1b09fed05d6d1
SydneyAntoni <sydney.antoni@xapling.de>
2024-01-24 15:17:03 +0100

    URL

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -12,7 +12,7 @@ export default function NavItem({ name }: { name: string }) {
     );
   } else {
     return (
-      <a className={styles.itemNav} href={"/prototyp-next.js/" + name}>
+      <a className={styles.itemNav} href={"" + name}>
         {name === "" ? "xapling" : name}
       </a>
     );
