a80ce418633cd435a3dee6c90ac8e88c41b2a0e1
SydneyAntoni <sydney.antoni@xapling.de>
2024-04-18 15:18:15 +0200

    format

--- app/Portfolio/page.tsx
+++ app/Portfolio/page.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
-  // let baseSrc = "/prototyp-next.js";
-  let baseSrc = "http://localhost:3000/";
-  return (
-    <Image
-      src={baseSrc + "/alle.png"}
-      alt={"Portfolio"}
-      width={"1188"}
-      height={"509"}
-      unoptimized={true}
-      className={styles.portfolio}
-    />
-  );
+	// let baseSrc = "/prototyp-next.js";
+	let baseSrc = 'http://localhost:3000/';
+	return (
+		<Image
+			src={baseSrc + '/alle.png'}
+			alt={'Portfolio'}
+			width={'1188'}
+			height={'509'}
+			unoptimized={true}
+			className={styles.portfolio}
+		/>
+	);
 }
--- app/components/Link/Link.tsx
+++ app/components/Link/Link.tsx
@@ -1,23 +1,23 @@
-"use client";
-import { usePathname } from "next/navigation";
-import styles from "@/app/layout.module.css";
-import React, { ReactNode } from "react";
+'use client';
+import { usePathname } from 'next/navigation';
+import styles from '@/app/layout.module.css';
+import React, { ReactNode } from 'react';
 
 interface Props {
-  children: ReactNode;
-  link: string;
+	children: ReactNode;
+	link: string;
 }
 
 const Link = ({ children, link }: Props) => {
-  let currentUrl = usePathname();
-  return (
-    <a
-      className={currentUrl === link ? styles.itemNavSelected : styles.itemNav}
-      href={process.env.NEXT_PUBLIC_BASE_URL + link}
-    >
-      {children}
-    </a>
-  );
+	let currentUrl = usePathname();
+	return (
+		<a
+			className={currentUrl === link ? styles.itemNavSelected : styles.itemNav}
+			href={process.env.NEXT_PUBLIC_BASE_URL + link}
+		>
+			{children}
+		</a>
+	);
 };
 
 export default Link;
