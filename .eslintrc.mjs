65955a484400413a7ab0c88515537283bfac8959
SydneyAntoni <sydney.antoni@xapling.de>
2024-04-25 15:17:42 +0200

    updated linter workflow

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -1,14 +1,14 @@
 import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
-    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-    { languageOptions: { globals: globals.browser } },
-    {
-        plugins: {
-            prettier: pluginPrettier,
-        },
-        rules: {
-            'prettier/prettier': 'error',
-        },
-    },
+	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+	{ languageOptions: { globals: globals.browser } },
+	{
+		plugins: {
+			prettier: pluginPrettier,
+		},
+		rules: {
+			'prettier/prettier': 'error',
+		},
+	},
 ];
